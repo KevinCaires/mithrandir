107f01e9f49a342fe35afc1b5865f1ba1514d604
KevinCaires <51523923+KevinCaires@users.noreply.github.com>
2020-04-28 20:46:54 -0300

    ADD: JobGroup (#13)

--- jobs/schema.py
+++ jobs/schema.py
@@ -2,8 +2,14 @@ import django_filters
 import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-from jobs.models import Job
+from jobs.models import Job, JobGroup
 from mithrandir.tools import get_object_id
+
+class JobGroupFilter(django_filters.FilterSet):
+    class Meta:
+        model = JobGroup
+        fields = ['name']
+
 
 class JobFilter(django_filters.FilterSet):
     class Meta:
@@ -14,6 +20,12 @@ class JobFilter(django_filters.FilterSet):
 class JobNode(DjangoObjectType):
     class Meta:
         model = Job
+        interfaces = (graphene.relay.Node, )
+
+
+class JobGroupNode(DjangoObjectType):
+    class Meta:
+        model = JobGroup
         interfaces = (graphene.relay.Node, )
 
 
@@ -30,6 +42,10 @@ class Query(graphene.ObjectType):
         JobNode,
         filterset_class=JobFilter,
     )
+    job_group = DjangoFilterConnectionField(
+        JobGroupNode,
+        filterset_class=JobGroupFilter,
+    )
 
 
 ################################################################################################
@@ -39,6 +55,37 @@ class Query(graphene.ObjectType):
 #  \ \_\ \ \_\  \ \_____\    \ \_\  \ \_\ \_\    \ \_\  \ \_\  \ \_____\  \ \_\\"\_\  \/\_____\# 
 #   \/_/  \/_/   \/_____/     \/_/   \/_/\/_/     \/_/   \/_/   \/_____/   \/_/ \/_/   \/_____/# 
 ################################################################################################
+
+class CreateJobGroup(graphene.relay.ClientIDMutation):
+    job_group = graphene.Field(JobGroupNode)
+
+    class Input:
+        name = graphene.String(
+            description='Nome of group',
+            required=True,
+        )
+        description = graphene.String(
+            description="Group description",
+            required=True,
+        )
+    
+    def mutate_and_get_payload(root, info, **_input):  # pylint: disable=no-self-argument
+        name = _input.get('name')
+        description = _input.get('description')
+
+        if not name:
+            raise Exception('Name is required!')
+
+        if not description:
+            raise Exception('Description is required!')
+        job_group = JobGroup(
+            name=name,
+            description=description,
+        )
+        job_group.save()
+
+        return CreateJobGroup(job_group=job_group)
+
 
 class CreateJob(graphene.relay.ClientIDMutation):
     job = graphene.Field(JobNode)
@@ -55,21 +102,66 @@ class CreateJob(graphene.relay.ClientIDMutation):
         value_per_meter = graphene.Float(
             description='If can be charged per meter, what is the price of the meter?'
         )
-        job_group = graphene.String(
+        job_group_id = graphene.String(
             description='Group of job',
             required=True,
         )
 
     def mutate_and_get_payload(root, info, **_input):  # pylint: disable=no-self-argument
+        _id = get_object_id(_input.get('job_group_id'), 'JobGroupNode')
+        job_group_id = JobGroup.objects.get(pk=_id)  # pylint: disable=no-member
+
         job = Job(
             name=_input.get('name'),
             per_meter=_input.get('per_meter'),
             value_per_meter=_input.get('value_per_meter'),
-            job_group=_input.get('job_group'),
+            job_group=job_group_id,
         )
         job.save()
 
         return CreateJob(job=job)
+
+
+class UpdateJobGroup(graphene.relay.ClientIDMutation):
+    job_group = graphene.Field(JobGroupNode)
+
+    class Input:
+        id = graphene.String(
+            description='Group ID',
+            required=True,
+        )
+        name = graphene.String(
+            description="Group name",
+        )
+        description = graphene.String(
+            description="Group description",
+        )
+
+    def mutate_and_get_payload(root, info, **_input):  # pylint: disable=no-self-argument
+        _id = get_object_id(_input.get('id'), 'JobGroupNode')
+        name = _input.get('name')
+        description = _input.get('description')
+
+        if not _id:
+            raise Exception('Id is required')
+
+        job_groups = JobGroup.objects.get(pk=_id)  # pylint: disable=no-member
+
+        if not _input.get('name'):
+            name = job_groups.name
+        
+        if not _input.get('description'):
+            description = job_groups.description
+
+
+        job_group = JobGroup(
+            id=_id,
+            name=name,
+            description=description,
+        )
+        job_group.save()
+
+        return CreateJobGroup(job_group=job_group)
 
 
 class UpdateJob(graphene.relay.ClientIDMutation):
@@ -134,4 +226,6 @@ class UpdateJob(graphene.relay.ClientIDMutation):
 
 class Mutation(graphene.AbstractType):
     create_job = CreateJob.Field()
+    create_job_group = CreateJobGroup.Field()
     update_job = UpdateJob.Field()
+    update_job_group = UpdateJobGroup.Field()
