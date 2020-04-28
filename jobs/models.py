b16397dfcfc8a4025eefc5baea1c0f7938eb4fd1
KevinCaires <kevincaires@ymail.com>
2020-04-28 20:44:56 -0300

    ADD: JobGroup

--- jobs/models.py
+++ jobs/models.py
@@ -20,8 +20,48 @@ class Job(models.Model):
         null=False,
         default=False,
     )
-    job_group = models.CharField(
-        blank=False,
+    job_group = models.ForeignKey(
+        'JobGroup',
         null=False,
+        on_delete=models.CASCADE,
+    )
+
+
+class JobGroup(models.Model):
+    """
+    Grupos de serviço.
+    Para identificar em qual grupo um determinado serviço faz parte.
+    """
+    name = models.CharField(
+        null=False,
+        blank=False,
+        unique=True,
         max_length=50,
     )
+    description = models.TextField(
+        null=False,
+        blank=False,
+    )
+
+
+# class PersonalProtectiveEquipment(models.Model):
+#     """
+#     Equipamento de proteção individual.
+#     É obrigatório que os usuário prestadores dos serviços tenham o próprio equipamento.
+#     """
+#     name = models.CharField(
+#         null=False,
+#         blank=False,
+#         unique=True,
+#         max_length=50,
+#     )
+#     description = models.TextField(
+#         null=False,
+#         blank=False,
+#     )
+
+
+# class JobEquipment(models.Model):
+#     """
+#     Equipamentos necessários para que a atividade possa ser prestada.
+#     """
